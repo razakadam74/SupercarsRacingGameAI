d5e65d00fd0168750db0aad571c4ee79ec913c0a
Abdul-Razak Adam <a4lph4@gmail.com>
2018-06-08 23:34:52 +0300

    lockdown at drawing lanes

--- capture_screen.py
+++ capture_screen.py
@@ -55,9 +55,103 @@ def process_image(original_img):
     #                         threshold=160, lines=np.array([]), minLineLength=40, maxLineGap=25)
     lines = cv2.HoughLinesP(processed_img, 1, np.pi / 180, 180,np.array([]),200, 15)
 
+    try:
+        l1,l2 = draw_lanes(original_img, lines)
+        cv2.line(original_img, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
+        cv2.line(original_img, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
+    except Exception as e:
+        print(str(e))
+
+    try:
+        for coords in lines:
+            coords = coords[0]
+            try:
+                cv2.line(processed_img, (coords[0], coords[1]), (coords[2], coords[3]), [255,0,0], 3)
+            except Exception as e:
+                print(str(e))
+    except Exception as e:
+        print(str(e))
+
+    
     # draw line in processed_image
     draw_line(processed_img, lines)
+    # processed_img = draw_lanes(processed_img, lines)
     return processed_img
+
+def draw_lanes(img, lines, color=[0,255,255], thickness =3):
+    try:
+        ys = []
+        for i in lines:
+            for ii in i:
+                ys += [ii[1], ii[3]]
+        min_y = min(ys)
+        max_y = img.shape[0]
+        new_lines = []
+        line_dict = {}
+
+        for idx, i in enumerate(lines):
+            for xyxy in i:
+                x_coords = (xyxy[0], xyxy[2])
+                y_coords = (xyxy[1], xyxy[3])
+                A = np.vstack([x_coords, np.ones(len(x_coords))]).T
+                m,b = np.lstsq(A, y_coords)[0]
+
+                x1= (min_y - b) / m
+                x2 = (max_y - b) /m
+
+                line_dict[idx] = [m,b, [int(x1), min_y, int(x2), max_y]]
+                #new_lines.append([int(x1), min_y, int(x2),max_y])
+        final_lanes = {}
+
+        for idx in line_dict:
+            final_lanes_copy = final_lanes.copy()
+            m = line_dict[idx][0]
+            b = line_dict[idx][1]
+            line = line_dict[idx][2]
+
+            if len(final_lanes) == 0:
+                final_lanes[m] = [[m,b,line]]
+            else:
+                found_copy =False
+
+                for other_ms in final_lanes_copy:
+                    if not found_copy:
+                        if abs(other_ms * 1.2) > abs(m) > abs(other_ms * 0.9):
+                            if abs(final_lanes_copy[other_ms][0][1]*1.2) > abs(b) > abs(final_lanes_copy[other_ms][0][1]*0.8):
+                                final_lanes[other_ms].append([m,b,line])
+                                found_copy = True
+                                break
+                        else:
+                            final_lanes[m] = [[m,b,line]]
+        line_counter = {}
+
+        for lanes in final_lanes:
+            line_counter[lanes] = len(final_lanes[lanes])
+        
+        top_lanes = sorted(line_counter.items(), key= lambda item: item[1])[::-1][:2]
+
+        lane1_id = top_lanes[0][0]
+        lane2_id = top_lanes[1][0]
+
+        def average_lane(lane_data):
+            x1s = []
+            y1s = []
+            x2s = []
+            y2s = []
+
+            for data in lane_data:
+                x1s.append(data[2][0])
+                y1s.append(data[2][1])
+                x2s.append(data[2][2])
+                y2s.append(data[2][3])
+            return int(np.mean(x1s)), int(np.mean(y1s)), int(np.mean(x2s)), int(np.mean(y2s)) 
+
+        l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
+        l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
+
+        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
+    except Exception as e:
+        print(str(e))
 
 
 def record_screen():
